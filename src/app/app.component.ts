51dde36c092b4defb85157339222b31b606dcb06
Pradeep <pradeep.gupta@ajatus.co.in>
2019-07-28 23:21:23 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FileElement } from './fileExplorer/model/file-element';
+import { Observable } from 'rxjs';
+import { FileService } from './service/file.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fileExplorer';
+  fileElements: Observable<FileElement[]>;
+  currentRoot: FileElement;
+  currentPath: string;
+  canNavigateUp = false;
+
+  constructor(public fileService: FileService){}
+
+  ngOnInit(){}
+
+  addFolder(folder: { name: string }) {
+    this.fileService.add({ isFolder: true, name: folder.name, parent: this.currentRoot ? this.currentRoot.id : 'root' });
+    this.updateFileElementQuery();
+  }
+  
+  removeElement(element: FileElement) {
+    this.fileService.delete(element.id);
+    this.updateFileElementQuery();
+  }
+  
+  moveElement(event: { element: FileElement; moveTo: FileElement }) {
+    this.fileService.update(event.element.id, { parent: event.moveTo.id });
+    this.updateFileElementQuery();
+  }
+
+  updateFileElementQuery() {
+    this.fileElements = this.fileService.queryInFolder(this.currentRoot ? this.currentRoot.id : 'root');
+  }
+  
+  renameElement(element: FileElement) {
+    this.fileService.update(element.id, { name: element.name });
+    this.updateFileElementQuery();
+  }
+
+  navigateUp() {
+    if (this.currentRoot && this.currentRoot.parent === 'root') {
+      this.currentRoot = null;
+      this.canNavigateUp = false;
+      this.updateFileElementQuery();
+    } else {
+      this.currentRoot = this.fileService.get(this.currentRoot.parent);
+      this.updateFileElementQuery();
+    }
+    this.currentPath = this.popFromPath(this.currentPath);
+  }
+  
+  navigateToFolder(element: FileElement) {
+    this.currentRoot = element;
+    this.updateFileElementQuery();
+    this.currentPath = this.pushToPath(this.currentPath, element.name);
+    this.canNavigateUp = true;
+  }
+
+  pushToPath(path: string, folderName: string) {
+    let p = path ? path : '';
+    p += `${folderName}/`;
+    return p;
+  }
+  
+  popFromPath(path: string) {
+    let p = path ? path : '';
+    let split = p.split('/');
+    split.splice(split.length - 2, 1);
+    p = split.join('/');
+    return p;
+  }
 }
